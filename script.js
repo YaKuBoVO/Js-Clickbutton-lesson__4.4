703ada3f2c26f379de0f00637051949d18fbc85d
YaKuBoVO <oyakubov526@gmail.com>
2023-06-26 16:05:42 +0500

    Click button tayyor

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict"
+
+const body = document.querySelector('body');
+const elToggle = document.getElementById('toggle');
+const elDemo = document.getElementById('demo');
+const elSpan = document.querySelector('.color');
+const elColor = document.querySelector('.black');
+const elDye = document.querySelector('.dye');
+
+elToggle.onclick = function() {
+   elToggle.classList.toggle('active');
+   body.classList.toggle('active');
+   elDemo.classList.toggle('active');
+   elSpan.classList.toggle('active');
+   elColor.classList.toggle('active');
+   elDye.classList.toggle('active');
+}
